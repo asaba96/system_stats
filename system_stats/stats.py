affbdd0459a5957fbba4cd572c608522d04b9ba3
Autera8 <autera8@d.space>
2022-09-29 15:18:27 -0400

    add support for per core cpu usage

--- system_stats/stats.py
+++ system_stats/stats.py
@@ -6,7 +6,7 @@
 
 import psutil
 
-from system_stats.msg import Float32Stamped, UInt64Stamped
+from system_stats.msg import Float32Stamped, Float32StampedArray, UInt64Stamped
 
 from std_msgs.msg import Header
 from diagnostic_msgs.msg import DiagnosticArray, DiagnosticStatus, KeyValue
@@ -48,6 +48,8 @@ class SystemStats(object):
 
             self.cpu_pub = node.create_publisher(Float32Stamped, "system/cpu/usage", 10)
 
+            self.cpu_cores_pub = node.create_publisher(Float32StampedArray, "system/cpu/core/usage", 10)
+
             self.disk_pub = node.create_publisher(
                 Float32Stamped, "system/disk/usage", 10
             )
@@ -80,6 +82,8 @@ class SystemStats(object):
     def update(self):
         status_cpu = DiagnosticStatus()
         status_cpu.name = "CPU"
+        status_cpu_cores = DiagnosticStatus()
+        status_cpu_cores.name = "CPU Per Core"
         status_mem = DiagnosticStatus()
         status_mem.name = "Memory"
         status_disk = DiagnosticStatus()
@@ -95,6 +99,8 @@ class SystemStats(object):
             cpu_temp.header = header
             cpu = Float32Stamped()
             cpu.header = header
+            cpu_core = Float32StampedArray()
+            cpu_core.header = header
             disk = Float32Stamped()
             disk.header = header
             mem = Float32Stamped()
@@ -118,8 +124,10 @@ class SystemStats(object):
                 )
 
             # cpu usage
-            cpu_usage = mean(psutil.cpu_percent(percpu=True))
+            all_cores = psutil.cpu_percent(percpu=True)
+            cpu_usage = mean(all_cores)
             status_cpu.values.append(KeyValue(key="usage", value=str(cpu_usage)))
+            status_cpu_cores.values.append(KeyValue(key="per core usage", value=str(all_cores)))
 
             # disk
             disk_usage = psutil.disk_usage("/").percent
@@ -178,6 +186,7 @@ class SystemStats(object):
                 if cpu_coretemp is not None:
                     cpu_temp.data = cpu_coretemp
                 cpu.data = cpu_usage
+                cpu_core.data = all_cores
                 disk.data = disk_usage
                 mem.data = mem_usage_virtual
                 swap.data = mem_usage_swap
@@ -185,6 +194,7 @@ class SystemStats(object):
                 # publish
                 self.cpu_temp_pub.publish(cpu_temp)
                 self.cpu_pub.publish(cpu)
+                self.cpu_cores_pub.publish(cpu_core)
                 self.disk_pub.publish(disk)
                 self.mem_pub.publish(mem)
                 self.swap_pub.publish(swap)
